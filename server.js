877f1f90c2239e95419bd489a010ad1232941eba
Chandaluri <S530742@nwmissouri.edu>
2018-07-03 14:24:27 -0500

    Added POST , GET 's'

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var { mongoose } = require('./db/config');
+var { user } = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/user',(req,res) => {
+
+    var otherTodo = new user({
+        name: req.body.name
+    });    
+    otherTodo.save().then((result) =>{
+        res.send(JSON.stringify(result));
+    },(err) =>{
+        res.send(err);
+    })
+});
+
+app.get('/user',(req,res) => {
+    user.find().then((user) => {
+        res.send({user});
+    }, (e) => {
+        res.send(400);
+    })
+});
+
+app.get('/user/:id', (req, res) =>{
+
+    //testing what id we are getting
+  //  res.send(req.params);
+
+  var id = req.params.id;
+  
+  user.findById(id).then((detailsbyId) =>{
+    res.send({detailsbyId});
+  },(err) => {
+    res.send(400);
+  });
+
+
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+module.exports = {app};
+
+
+
